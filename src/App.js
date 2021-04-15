3678fd3c0f7c2dcb9799c8076b2406186813472b
Anand Mohan <51752139+anandmohan23@users.noreply.github.com>
2021-04-16 00:43:20 +0530

    create app for 1 user

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="20c88d14-494d-4298-88dc-0ea75b8d5ef3"
+            userName="admin1"
+            userSecret="admin1"
+        />
+    )
+}
+
+export default App;
